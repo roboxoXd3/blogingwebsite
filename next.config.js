8f083b3ff748c6d36906abf5e699119555394bd7
roboxoXd3 <rishavshahi87@gmail.com>
2024-07-01 01:28:21 +0530

    Configured image domains for Next.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['uialkfbsyxgduvgbwcxh.supabase.co'],
+    },
+  }
+  
+  module.exports = nextConfig
